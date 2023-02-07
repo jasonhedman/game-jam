a02066c2e48ecd95166a07388c8d7ddf824e55ff
gstuard <gstuard@hamilton.edu>
2023-02-07 12:57:03 +0100

    Update Cat.cs

--- Assets/Scripts/Cat.cs
+++ Assets/Scripts/Cat.cs
@@ -6,12 +6,21 @@ public class Cat : Player
 {
     internal float charge = 0;
     internal bool hitbox_exists = false;
+    internal float attack_timer = 0;
 
     public GameObject LeapHitbox;
 
     void Attack()
     {
-        speed = Mathf.Max(starting_speed, speed);
+        Debug.Log("Attacking!");
+        RaycastHit2D swipe = Physics2D.BoxCast(transform.position, new Vector2(1, 1), 45, new Vector2(0, direction), 2, 1 << 5);
+        Debug.DrawRay(transform.position, new Vector2(1, 1), Color.red, 2);
+        if (swipe.collider != null)
+        {
+            Debug.Log(swipe.collider);
+            Destroy(swipe.collider);
+        }
+
         //Instantiate(LeapHitbox);
         //hitbox_exists = true;
         //Physics2D.IgnoreCollision(GetComponent<Collider2D>(), LeapHitbox.GetComponent<Collider2D>());
@@ -26,6 +35,12 @@ public class Cat : Player
     void Update()
     {
         RunPhysics();
+
+        if (attack_timer > 0)
+        {
+            attack_timer -= Time.deltaTime;
+            Attack();
+        }
 
         //if (hitbox_exists)
         //{
@@ -60,6 +75,8 @@ public class Cat : Player
             else
             {
                 Attack();
+                attack_timer = 0.5f;
+                speed = Mathf.Max(starting_speed, speed); ();
             }
         }
     }
